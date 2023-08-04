aa32f54010781a43dad2e598a4772e3de6e3e9a1
Paweł Kowaluk <pensjonatus@gmail.com>
2023-08-04 20:57:27 +0200

    display event meta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,6 +43,7 @@ const config = {
           path: 'events',
           routeBasePath: 'events',
           sidebarPath: require.resolve('./sidebars.js'),
+          breadcrumbs: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
