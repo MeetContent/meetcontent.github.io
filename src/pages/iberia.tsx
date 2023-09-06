ad9287d366a70574e23d68e836284310e4d917a4
dbarr <dbarr@dolby.com>
2023-09-06 20:29:42 +0200

    Update iberia.tsx

--- src/pages/iberia.tsx
+++ src/pages/iberia.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import DivisionPage from '../components/DivisionPage/DivisionPage';
+import DivisionPageIberia from '../components/DivisionPage/DivisionPage';
 
 export default function Iberia() {
   return (
     <DivisionPageIberia
       name="Iberia"
-      description="Meetups in Spain and Portugal!"
-      about={<>A few words from Daniel will be here...</>}
+      description="Meetups en España y Portugal"
+      about={<>
+      Aquí viene nuestro texto. 1, 2, 3, probando...
+      </>}
       blogLink="blog/tags/iberia"
       meetupsLink="events/iberia"
       newsletterLink=""
