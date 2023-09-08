3f3363708422d4553be29e1d8402fd2e7b9119b9
dbarr <dbarr@dolby.com>
2023-09-08 19:16:23 +0200

    bringing back the validation script

--- src/components/DivisionPage/MailchimpEmbedIberia.tsx
+++ src/components/DivisionPage/MailchimpEmbedIberia.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const MailchimpValidationScript = `
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+`
+
 export default function MailchimpEmbedIberia() {
   return (
     <div id="mc_embed_shell">
@@ -175,6 +179,7 @@ export default function MailchimpEmbedIberia() {
           </div>
         </form>
       </div>
+      <div dangerouslySetInnerHTML={{__html: MailchimpValidationScript}}/>
     </div>
   );
 }
