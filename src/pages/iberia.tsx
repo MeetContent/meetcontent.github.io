8262146355beb602bc264604f7330573a9d77705
Sara Sanchez <138022085+SaraSanchezOr@users.noreply.github.com>
2023-09-12 11:37:13 +0200

    Update iberia.tsx
    Updated Daniel Barrios

--- src/pages/iberia.tsx
+++ src/pages/iberia.tsx
@@ -7,10 +7,15 @@ export default function Iberia() {
     <DivisionPageIberia
       name="Iberia"
       description="Meetups en España y Portugal"
-      about={<>
-      Aquí viene nuestro texto. 1, 2, 3, probando...test
-      <p>Aquí viene un párrafo</p>
-      </>}
+      about={
+        <>
+          <h1>Daniel Barrios</h1>
+          <p>
+            Daniel viene del mundo del software. Lleva trabajando más de una década en contenidos técnicos, desde la creación de taxonomías para Google a la creación de documentación y herramientas de publicación en Nokia o Dolby. Hasta 2020 Daniel fue coordinador de MeetContent Wrocław, y fue uno de los creadores del primer curso de postgrado de comunicación técnica en Polonia.
+          </p>
+          <img src="iberia-daniel.jpg" alt="DanielBarrios" />
+        </>
+      }
       blogLink="blog/tags/iberia"
       meetupsLink="events/iberia"
       newsletterLink=""
