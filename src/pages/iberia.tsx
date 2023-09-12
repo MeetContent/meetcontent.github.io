68f3c44e05704115bc3959395d0c1812781ac1db
Sara Sanchez <138022085+SaraSanchezOr@users.noreply.github.com>
2023-09-12 12:26:54 +0200

    Update iberia.tsx

--- src/pages/iberia.tsx
+++ src/pages/iberia.tsx
@@ -9,21 +9,28 @@ export default function Iberia() {
       description="Meetups en España y Portugal"
       about={
         <>
-          <div>
-            <h1>Daniel Barrios</h1>
-            ![DanielBarrios](static/img/iberia-daniel.jpg)
-            <img src="iberia-daniel.jpg" alt="Daniel Barrios" />
-            <p>
-              Daniel viene del mundo del software y tiene más de una década de experiencia en contenidos técnicos, que incluye la creación de taxonomías para Google, la generación de documentación y herramientas de publicación en empresas como Nokia y Dolby. Hasta 2020, Daniel fue coordinador de MeetContent Wrocław y uno de los creadores del primer curso de postgrado de comunicación técnica en Polonia.
-            </p>
-          </div>
-          <div>
-            <h2>Sara Sánchez</h2>
-            <img src="iberia-sara.jpg" alt="Sara Sánchez" />
-            <p>
-              Sara tiene experiencia en traducción técnica, que abarca desde manuales técnicos hasta documentos de patentes y nuevas tecnologías, con un enfoque especial en el software. Actualmente, desempeña un papel fundamental como Redactora Técnica en Omada, donde lidera el proyecto de Localización del software en ocho idiomas diferentes.
-            </p>
-          </div>
+          <table>
+            <tr>
+              <td>
+                <div>
+                  <h1>Daniel Barrios</h1>
+                  <img src="iberia-daniel.jpg" alt="Daniel Barrios" />
+                  <p>
+                    Daniel viene del mundo del software y tiene más de una década de experiencia en contenidos técnicos, que incluye la creación de taxonomías para Google, la generación de documentación y herramientas de publicación en empresas como Nokia y Dolby. Hasta 2020, Daniel fue coordinador de MeetContent Wrocław y uno de los creadores del primer curso de postgrado de comunicación técnica en Polonia.
+                  </p>
+                </div>
+              </td>
+              <td>
+                <div>
+                  <h2>Sara Sánchez</h2>
+                  <img src="iberia-sara.jpg" alt="Sara Sánchez" />
+                  <p>
+                    Sara tiene experiencia en traducción técnica, que abarca desde manuales técnicos hasta documentos de patentes y nuevas tecnologías, con un enfoque especial en el software. Actualmente, desempeña un papel fundamental como Redactora Técnica en Omada, donde lidera el proyecto de Localización del software en ocho idiomas diferentes.
+                  </p>
+                </div>
+              </td>
+            </tr>
+          </table>
         </>
       }
       blogLink="blog/tags/iberia"
@@ -32,3 +39,4 @@ export default function Iberia() {
     />
   );
 }
+
