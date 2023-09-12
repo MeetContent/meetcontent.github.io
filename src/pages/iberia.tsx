0858c264592a3762b46cf0efb61c17bb49ef8b80
dbarr <dbarr@dolby.com>
2023-09-12 19:21:45 +0200

    Paths for the bio-pictures

--- src/pages/iberia.tsx
+++ src/pages/iberia.tsx
@@ -11,14 +11,14 @@ export default function Iberia() {
         <>
           <div>
             <h1>Daniel Barrio Fierro</h1>
-            <img src="img/iberia-daniel.png" alt="Daniel Barrio Fierro" className="bio-picture"/>
+            <img src="/img/iberia-daniel.png" alt="Daniel Barrio Fierro" className="bio-picture"/>
             <p>
               Daniel viene del mundo del software y tiene más de una década de experiencia en contenidos técnicos, incluyendo la creación de taxonomías para Google o la generación de documentación y herramientas de publicación en Nokia y Dolby. Hasta 2020 Daniel fue coordinador de MeetContent Wrocław y uno de los creadores del primer curso de postgrado de comunicación técnica en Polonia.
             </p>
           </div>
           <div>
             <h1>Sara Sánchez</h1>
-            <img src="img/iberia-sara.png" alt="Sara Sánchez" className="bio-picture"/>
+            <img src="/img/iberia-sara.png" alt="Sara Sánchez" className="bio-picture"/>
             <p>
               Sara tiene experiencia en traducción técnica, que abarca desde manuales técnicos hasta documentos de patentes y nuevas tecnologías, con un enfoque especial en el software. Actualmente desempeña un papel fundamental como Redactora Técnica en Omada, donde lidera el proyecto de Localización del software en ocho idiomas diferentes.
             </p>
