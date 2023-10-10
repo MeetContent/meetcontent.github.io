e0bd540a7951ac7a8e4a9a1079b465c5a0ed4ac3
dbarr <dbarr@dolby.com>
2023-10-10 14:13:19 +0200

    Better social card image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,7 +147,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/meet-content-blue.png',
+      image: 'img/logo_transparent_blue_400.png',
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true,
